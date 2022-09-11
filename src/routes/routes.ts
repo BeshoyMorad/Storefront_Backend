163ec9f2290368d6b5fbf1fe1e223cc32b8e6794
BeshoyMorad <besh0morta@gmail.com>
2022-09-12 00:06:49 +0200

    Added main packages and folders

---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+//routes
+
+const routes = express.Router();
+
+// routes.use();
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import routes from "./routes/routes";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/api", routes);
+
+app.get("/", function (_req: Request, res: Response) {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`starting app on: ${port}`));
