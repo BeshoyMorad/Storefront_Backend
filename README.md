f7d62561b6c3b5556adca0ad8508c2896d5e17f2
BeshoyMorad <besh0morta@gmail.com>
2022-09-12 14:00:30 +0200

    created database and added environment variables

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Storefront_Backend
 
 Second Project of Udacity Scholarship Advanced Full-Stack Web Development
+
+CREATE USER full_stack_user WITH PASSWORD '123';
+CREATE DATABASE store_front;
+\c store_front
+GRANT ALL PRIVILEGES ON DATABASE store_front TO full_stack_user;
+
+CREATE DATABASE store_front_test;
+\c store_front_test
+GRANT ALL PRIVILEGES ON DATABASE store_front_test TO full_stack_user;
---
+++ src/database.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+dotenv.config();
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
+
+let client = new Pool();
+
+if (ENV === "dev") {
+  client = new Pool({
+    database: POSTGRES_DB,
+    host: POSTGRES_HOST,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+if (ENV?.trim() === "test") {
+  client = new Pool({
+    database: POSTGRES_TEST_DB,
+    host: POSTGRES_HOST,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+export default client;
