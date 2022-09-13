6f77210ef6541cd1f773e9e05ca2df6d1c7e56d6
BeshoyMorad <besh0morta@gmail.com>
2022-09-13 16:40:10 +0200

    CRUD operations for product model

--- src/models/product.ts
+++ src/models/product.ts
@@ -7,4 +7,83 @@ export type Product = {
   category: string;
 };
 
-export class ProductStore {}
+export class ProductStore {
+  async create(p: Product): Promise<Product> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        "INSERT INTO products(name, price, category) VALUES($1, $2, $3) RETURNING *;";
+
+      const result = await conn.query(sql, [p.name, p.price, p.category]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Cannot add new product ${p.name} : ${error}`);
+    }
+  }
+
+  async index(): Promise<Product[]> {
+    try {
+      const conn = await client.connect();
+      const sql = "SELECT * FROM products;";
+
+      const result = await conn.query(sql);
+
+      conn.release();
+
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Cannot index all products : ${error}`);
+    }
+  }
+
+  async show(id: number): Promise<Product> {
+    try {
+      const conn = await client.connect();
+      const sql = "SELECT * FROM products WHERE id=$1;";
+
+      const result = await conn.query(sql, [id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Cannot get product with id ${id} : ${error}`);
+    }
+  }
+
+  async update(p: Product): Promise<Product> {
+    try {
+      const conn = await client.connect();
+      // id: number, name: varchar, price: number, category: varchar
+
+      const sql =
+        "UPDATE products SET name=$1, price=$2, category=$3 WHERE id=$4 RETURNING *;";
+
+      const result = await conn.query(sql, [p.name, p.price, p.category, p.id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Cannot update product ${p.name} : ${error}`);
+    }
+  }
+
+  async destroy(id: number): Promise<Product> {
+    try {
+      const conn = await client.connect();
+      const sql = "DELETE FROM products WHERE id=$1;";
+
+      const result = await conn.query(sql, [id]);
+
+      conn.release();
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error(`Cannot delete product with id ${id} : ${error}`);
+    }
+  }
+}
